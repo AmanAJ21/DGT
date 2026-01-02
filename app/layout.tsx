1550e185420ccfac32e9ddb3c5ec1f1f624e56e6
AmanJ <165487083+AmanAJ21@users.noreply.github.com>
2026-01-02 15:54:17 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Google AdSense */}
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8069198795330862"
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8069198795330862"
           crossorigin="anonymous" />
         {/* Google tag (gtag.js) - Optional for better tracking */}
         <Script
